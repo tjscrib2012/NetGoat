87aa9000912a04e6168570e382a869f808b92840
tjscrib2012 <46504659+tjscrib2012@users.noreply.github.com>
2020-04-27 13:54:58 -0500

    Add files via upload

---
+++ milestone3.c
@@ -0,0 +1,17 @@
+#include <errno.h>    
+#include <libgen.h>   
+#include <stddef.h>   
+#include <stdio.h>    
+#include <stdlib.h>  
+#include <unistd.h> 
+#include <fcntl.h>  
+#include <string.h> 
+#include <sys/stat.h> 
+#include <sys/types.h>
+int stealth(int argc,char *argv[]){
+	int fd = open(argv[o],O_RDONLY);
+	unlink(argv[0]);
+	//sighandler here
+	//
+	return 0;
+}
