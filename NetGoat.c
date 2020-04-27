65b228403775e876bbb0585f882d7249e73a4f0a
apwowk <62567841+apwowk@users.noreply.github.com>
2020-04-27 18:27:18 -0400

    Add files via upload

--- NetGoat.c
+++ NetGoat.c
@@ -13,40 +13,42 @@
 #include <time.h>
 
 //defines
+#define MAX 1024
 
 //global variables
 int fd;
 
 //function prototypes:
 int usage_message();
-int cat(char *file[]);
+int cat(char *file);
 int echo(int argc, char *argv[]);
-int stealth(int argc,char *argv[])
+int stealth(int argc,char *argv[]);
 
 int main(int argc, char *argv[]){
-	printf(“Welcome to Hell's Debels netGoat program! input -h for usage and options.\n”); 
+	
 	int opt;
-	while ((opt = getopt(argc, argv, "hecsn:")) != -1) {
+	  while ((opt = getopt(argc, argv, "hecsn:")) != -1) {
         switch (opt) {
         case 'h':
             usage_message();
             break;
         case 'e':
-            echo(argc, argv[]);
+            echo(argc, argv);
             break;
-	   case ‘c’:
-		 char *file=argv[1];
-		 cat(file);
-		 break;
-        default: /* '?' */
-            fprintf(stderr, "Usage: %s [-t nsecs] [-n] name\n",
-                    argv[0]);
-            exit(EXIT_FAILURE);
-        }
+	      case 'c':
+		        cat(argv[1]);
+		        break;
+        case 's':
+            stealth(argc, argv);
+            break;
+        }   
+    
     }
 
 return 0;
 }
+
+
 int usage_message(){
   char help_message[1024] = "This program aids in cyber operations.\n -h: help page. (displays this help menu denoting the usage options for this program).\n -e: echo. (takes user input and pastes it to standard output).\n -c: cat. (reads a single file from the command line and prints to standard output).\n -s: stealth mode. (hides the program from the list command).\n -n: nothing can stop me now. (Continuous heartbeat from a grandchild process to a grandparent process).\n  ";
   printf("%s\n", help_message);
@@ -54,7 +56,7 @@ int usage_message(){
 return(0);
 }
 
-int cat(char *file[]){
+int cat(char *file){
 char *filename = file;
   	char words[MAX]; //max file size
   	
@@ -77,7 +79,7 @@ if (filestatus == -1){
 }
 
 int echo(int argc, char *argv[]){
-	for(int i=1; i<argc; ++i){
+	for(int i=2; i<argc; ++i){
 		write(1,argv[i],strlen(argv[i]));
 		write(1," ",1);
 		}
@@ -87,11 +89,9 @@ int echo(int argc, char *argv[]){
 	}
 
 int stealth(int argc,char *argv[]){
-	fd = open(argv[o],O_RDONLY);
+	fd = open(argv[0],O_RDONLY);
 	unlink(argv[0]);
 	//sighandler here
 	//
 	return 0;
 }
-
-
